6b6d206fb36e0c8a3fe56139c777a78274d45b6a
vanessa-watyla <vanessawattyla12@gmail.com>
2024-09-30 14:50:12 -0300

    feat: add configuração

--- app/views.py
+++ app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def calculator_view(request):
+    resultado = None
+    operacao = None
+
+    if request.method == "POST":
+        num1 = float(request.POST.get("num1"))
+        num2 = float(request.POST.get("num2"))
+        operacao = request.POST.get("operacao")
+
+        if operacao == "adicionar":
+            resultado = num1 + num2
+        elif operacao == "subtrair":
+            resultado = num1 - num2
+        elif operacao == "multiplicar":
+            resultado = num1 * num2
+        elif operacao == "dividir":
+            if num2 != 0:
+                resultado = num1 / num2
+            else:
+                resultado = "Erro: Divisão por zero"
+
+    return render(request, 'calculator.html', {'resultado': resultado, 'operacao': operacao})
